0637afd03077238d35b3fcab067b0e551e92d10b
mike-goodwin <mike.goodwin@owaps.org>
2016-09-07 18:03:33 +0100

    fix and tests for submit and inout buttons

--- test/buttons.js
+++ test/buttons.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 var path = require('path');
 
 // Write fixture data
-var mixins = ["btn-primary","btn-info","btn-warning","btn-danger","btn-success","btn-lg-primary","btn-lg-info","btn-lg-warning","btn-lg-danger","btn-lg-success" ];
-mixins.forEach(function(m) {
+var mixins = ["btn-primary", "btn-info", "btn-warning", "btn-danger", "btn-success", "btn-lg-primary", "btn-lg-info", "btn-lg-warning", "btn-lg-danger", "btn-lg-success"];
+mixins.forEach(function (m) {
     var fileTemplate = `include ../../../components/buttons
 +${m}(name)`;
     var fileName = `${m}.pug`;
-    fs.writeFileSync(path.join(__dirname,"fixtures/buttons",fileName),fileTemplate);
+    fs.writeFileSync(path.join(__dirname, "fixtures/buttons", fileName), fileTemplate);
 });
 
 
@@ -85,13 +85,83 @@ var testData = [
         locals: { name: "Large Success Button" },
         actual: '<button class="btn btn-success btn-lg" type="button">Large Success Button</button>'
     },
+    {
+        suite: 'input-btn-primary mixin',
+        spec: 'should render a input primary button',
+        fixture: 'input-btn-primary.pug',
+        locals: { name: "Input Primary Button" },
+        actual: '<input class="btn btn-primary" type="button" value="Input Primary Button"/>'
+    },
+    {
+        suite: 'input-btn-info mixin',
+        spec: 'should render a input info button',
+        fixture: 'input-btn-info.pug',
+        locals: { name: "Input Info Button" },
+        actual: '<input class="btn btn-info" type="button" value="Input Info Button"/>'
+    },
+    {
+        suite: 'input-btn-warning mixin',
+        spec: 'should render a input warning button',
+        fixture: 'input-btn-warning.pug',
+        locals: { name: "Input Warning Button" },
+        actual: '<input class="btn btn-warning" type="button" value="Input Warning Button"/>'
+    },
+    {
+        suite: 'input-btn-danger mixin',
+        spec: 'should render a input primary button',
+        fixture: 'input-btn-primary.pug',
+        locals: { name: "Input Danger Button" },
+        actual: '<input class="btn btn-primary" type="button" value="Input Danger Button"/>'
+    },
+    {
+        suite: 'input-btn-success mixin',
+        spec: 'should render a input success button',
+        fixture: 'input-btn-success.pug',
+        locals: { name: "Input Success Button" },
+        actual: '<input class="btn btn-success" type="button" value="Input Success Button"/>'
+    },
+    {
+        suite: 'submit-btn-primary mixin',
+        spec: 'should render a submit primary button',
+        fixture: 'submit-primary.pug',
+        locals: { name: "Submit Primary Button" },
+        actual: '<input class="btn btn-primary" type="submit" value="Submit Primary Button"/>'
+    },
+    {
+        suite: 'submit-btn-info mixin',
+        spec: 'should render a submit info button',
+        fixture: 'submit-info.pug',
+        locals: { name: "Submit Info Button" },
+        actual: '<input class="btn btn-info" type="submit" value="Submit Info Button"/>'
+    },
+    {
+        suite: 'submit-btn-warning mixin',
+        spec: 'should render a submit warning button',
+        fixture: 'submit-warning.pug',
+        locals: { name: "Submit Warning Button" },
+        actual: '<input class="btn btn-warning" type="submit" value="Submit Warning Button"/>'
+    },
+    {
+        suite: 'submit-btn-danger mixin',
+        spec: 'should render a input primary button',
+        fixture: 'submit-primary.pug',
+        locals: { name: "Submit Danger Button" },
+        actual: '<input class="btn btn-primary" type="submit" value="Submit Danger Button"/>'
+    },
+    {
+        suite: 'submit-btn-success mixin',
+        spec: 'should render a input success button',
+        fixture: 'submit-success.pug',
+        locals: { name: "Submit Success Button" },
+        actual: '<input class="btn btn-success" type="submit" value="Submit Success Button"/>'
+    }
 ];
 
-testData.forEach(function(item) {
-    describe(item.suite,function() {
-        it(item.spec,function() {
-            var fn = pug.compileFile(path.join(__dirname,"fixtures/buttons", item.fixture));
-            assert.equal(item.actual,fn(item.locals));
+testData.forEach(function (item) {
+    describe(item.suite, function () {
+        it(item.spec, function () {
+            var fn = pug.compileFile(path.join(__dirname, "fixtures/buttons", item.fixture));
+            assert.equal(item.actual, fn(item.locals));
         });
     });
 });
