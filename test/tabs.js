8dcfb03db67c3a30903ed03baadf599116e5db0b
thylux <thylux@gmail.com>
2017-04-15 23:21:37 +0100

    tabs with nav-pills
    Based on https://codepen.io/wizly/pen/BlKxo example

--- test/tabs.js
+++ test/tabs.js
@@ -48,6 +48,48 @@ describe('Tabs', function () {
         assert.equal(fn(locals), expected);
     });
 
+    it('should render nav pills headers without content', function () {
+        var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab-pills-empty.pug'));
+        var expected = '<ul class="nav nav-pills" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content clearfix"></div>';
+        var locals = {
+            tabs: items,
+            active: 1
+        };
+        assert.equal(fn(locals), expected);
+    });
+
+    it('should render nav pills headers with custom styles without content', function () {
+        var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab-pills-empty.pug'));
+        var expected = '<ul class="nav nav-pills test" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content clearfix"></div>';
+        var locals = {
+            tabs: items,
+            active: 1,
+            options: { style: "test" }
+        };
+        assert.equal(fn(locals), expected);
+    });
+
+    it('should render nav pills with content', function () {
+        var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab-pills.pug'));
+        var expected = '<ul class="nav nav-pills" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content clearfix"><div class="nav-pane active" id="tabitem1"></div><div class="nav-pane" id="tabitem2"></div><div class="nav-pane test" id="tabitem3"></div></div>';
+        var locals = {
+            tabs: items,
+            active: 1
+        };
+        assert.equal(fn(locals), expected);
+    });
+
+    it('should render nav pills headers with custom styles and content', function () {
+        var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab-pills.pug'));
+        var expected = '<ul class="nav nav-pills test" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content clearfix"><div class="nav-pane active" id="tabitem1"></div><div class="nav-pane" id="tabitem2"></div><div class="nav-pane test" id="tabitem3"></div></div>';
+        var locals = {
+            tabs: items,
+            active: 1,
+            options: { style: "test" }
+        };
+        assert.equal(fn(locals), expected);
+    });
+
     it('should render active tab', function () {
         var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab.pug'));
         var expected = '<div class="nav-pane active" id="tabtest"></div>';
