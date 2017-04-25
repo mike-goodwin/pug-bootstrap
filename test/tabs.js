21ee1276a8138a796ba0978103bca0f75d265334
Liu, Xiao <lio4072@hotmail.com>
2017-04-25 20:10:43 +0900

    modified the test/tabs.js to fit the test case

--- test/tabs.js
+++ test/tabs.js
@@ -29,7 +29,7 @@ describe('Tabs', function () {
 
     it('should render nav tabs with content', function () {
         var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab-list.pug'));
-        var expected = '<ul class="nav nav-tabs" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content"><div class="nav-pane active" id="tabitem1"></div><div class="nav-pane" id="tabitem2"></div><div class="nav-pane test" id="tabitem3"></div></div>';
+        var expected = '<ul class="nav nav-tabs" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content"><div class="tab-pane active" id="tabitem1"></div><div class="tab-pane" id="tabitem2"></div><div class="tab-pane test" id="tabitem3"></div></div>';
         var locals = {
             tabs: items,
             active: 1
@@ -39,7 +39,7 @@ describe('Tabs', function () {
 
     it('should render nav tabs headers with custom styles and content', function () {
         var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab-list.pug'));
-        var expected = '<ul class="nav nav-tabs test" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content"><div class="nav-pane active" id="tabitem1"></div><div class="nav-pane" id="tabitem2"></div><div class="nav-pane test" id="tabitem3"></div></div>';
+        var expected = '<ul class="nav nav-tabs test" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content"><div class="tab-pane active" id="tabitem1"></div><div class="tab-pane" id="tabitem2"></div><div class="tab-pane test" id="tabitem3"></div></div>';
         var locals = {
             tabs: items,
             active: 1,
@@ -71,7 +71,7 @@ describe('Tabs', function () {
 
     it('should render nav pills with content', function () {
         var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab-pills.pug'));
-        var expected = '<ul class="nav nav-pills" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content clearfix"><div class="nav-pane active" id="tabitem1"></div><div class="nav-pane" id="tabitem2"></div><div class="nav-pane test" id="tabitem3"></div></div>';
+        var expected = '<ul class="nav nav-pills" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content clearfix"><div class="tab-pane active" id="tabitem1"></div><div class="tab-pane" id="tabitem2"></div><div class="tab-pane test" id="tabitem3"></div></div>';
         var locals = {
             tabs: items,
             active: 1
@@ -81,7 +81,7 @@ describe('Tabs', function () {
 
     it('should render nav pills headers with custom styles and content', function () {
         var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab-pills.pug'));
-        var expected = '<ul class="nav nav-pills test" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content clearfix"><div class="nav-pane active" id="tabitem1"></div><div class="nav-pane" id="tabitem2"></div><div class="nav-pane test" id="tabitem3"></div></div>';
+        var expected = '<ul class="nav nav-pills test" role="tablist"><li><a href="#tabitem1" role="tab" data-toggle="tab">item1</a></li><li class="active"><a href="#tabitem2" role="tab" data-toggle="tab">item2</a></li><li><a href="#tabitem3" role="tab" data-toggle="tab">item3</a></li></ul><div class="tab-content clearfix"><div class="tab-pane active" id="tabitem1"></div><div class="tab-pane" id="tabitem2"></div><div class="tab-pane test" id="tabitem3"></div></div>';
         var locals = {
             tabs: items,
             active: 1,
@@ -92,7 +92,7 @@ describe('Tabs', function () {
 
     it('should render active tab', function () {
         var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab.pug'));
-        var expected = '<div class="nav-pane active" id="tabtest"></div>';
+        var expected = '<div class="tab-pane active" id="tabtest"></div>';
         var locals = {
             id: 'test',
             active: true
@@ -102,7 +102,7 @@ describe('Tabs', function () {
 
     it('should render tab with custom styles', function () {
         var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab.pug'));
-        var expected = '<div class="nav-pane test" id="tabtest"></div>';
+        var expected = '<div class="tab-pane test" id="tabtest"></div>';
         var locals = {
             id: 'test',
             active: false,
@@ -113,7 +113,7 @@ describe('Tabs', function () {
 
     it('should render active tab with custom styles', function () {
         var fn = pug.compileFile(path.join(__dirname, "fixtures/tabs", 'tab.pug'));
-        var expected = '<div class="nav-pane test active" id="tabtest"></div>';
+        var expected = '<div class="tab-pane test active" id="tabtest"></div>';
         var locals = {
             id: 'test',
             active: true,
